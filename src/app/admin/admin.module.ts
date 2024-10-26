aab963429f34f5e3a09ecbabf396f0af5501f892
mivanac <marivan0401@gmail.com>
2024-10-27 01:56:10 +0200

    First commit

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { DemoAngularMaterailModule } from '../DemoAngularMaterialModule';
+import { PostCategoryComponent } from './components/post-category/post-category.component';
+import { PostProductComponent } from './components/post-product/post-product.component';
+import { PostCouponComponent } from './components/post-coupon/post-coupon.component';
+import { CouponsComponent } from './components/coupons/coupons.component';
+
+
+@NgModule({
+  declarations: [
+    AdminComponent,
+    DashboardComponent,
+    PostCategoryComponent,
+    PostProductComponent,
+    PostCouponComponent,
+    CouponsComponent,
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    DemoAngularMaterailModule,
+  ]
+})
+export class AdminModule { }
